1d24f0a72271f2030dac5e9e56335cf130206b68
Kostas <mousik@gmail.com>
2015-03-04 21:37:37 +0000

    models

--- TripShare/models.py
+++ TripShare/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+git 
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
@@ -8,6 +8,8 @@ class UserProfile(models.Model):
     # The additional attributes
     driver = models.BooleanField(default=False)
     hasCar = models.BooleanField(default=False)
+    numRatings = models.IntegerField(default=0)
+    avgRating = models.FloatField(null=True)
 
     def __unicode__(self):
         return self.user.username
@@ -23,6 +25,27 @@ class Trip(models.Model):
     def __unicode__(self):
         return self.desc
 
+class Ratings(models.Model):
+    userRater = models.ForeignKey(UserProfile, related_name='rating_user')
+    userRated = models.ForeignKey(UserProfile, related_name='rated_user')
+    rating = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('userRater', 'userRated')
+
+class Requests(models.Model):
+    user = models.ForeignKey(UserProfile)
+    trip = models.ForeignKey(Trip)
+
+    class Meta:
+        unique_together = ('user', 'trip')
+
 class TripUsers(models.Model):
     user = models.ForeignKey(UserProfile)
     trip = models.ForeignKey(Trip)
+    driver = models.BooleanField(default=False)
+    usersCar = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('user', 'trip')
+
