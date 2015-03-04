1f603606cc7397c4322649ed2d1065714af03e70
Evangelos Karvounis <karvounis.b@gmail.com>
2015-03-04 23:00:33 +0000

    asdqdqd

--- TripShare/urls.py
+++ TripShare/urls.py
@@ -3,4 +3,5 @@ from TripShare import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
     )
--- TripShare/views.py
+++ TripShare/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     context_dict = {}
     return render(request, 'index.html', context_dict)
+
+def about(request):
+    return render(request, 'about.html', {})
