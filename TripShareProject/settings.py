5a53bd3dad2081c500bd1d93e4b77d98b48d7a34
unknown <2163659k@BO1028-4-04.ad.dcs.gla.ac.uk>
2015-03-05 15:38:41 +0000

    awqwq

--- TripShareProject/settings.py
+++ TripShareProject/settings.py
@@ -13,6 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -94,3 +95,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
+
+MEDIA_URL = '/media/'
