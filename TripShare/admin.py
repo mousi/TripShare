40f8af3a818dfe538d75fba2c8665d026301444f
Kostas <mousik@gmail.com>
2015-03-06 19:02:32 +0000

    admin

--- TripShare/admin.py
+++ TripShare/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from TripShare.models import User, UserProfile, Trip, TripUsers, Ratings, Requests
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(TripUsers)
+admin.site.register(Trip)
+admin.site.register(Ratings)
+admin.site.register(Requests)
