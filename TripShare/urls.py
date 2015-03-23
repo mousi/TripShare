e36f43a8bd4e99d4676677b66a9db1dbd55acd3d
Kostas <mousik@gmail.com>
2015-03-23 18:07:39 +0000

    Notifications

--- TripShare/urls.py
+++ TripShare/urls.py
@@ -4,7 +4,6 @@ from TripShare import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^test/$', views.test, name='test'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^register/$', views.register, name='register'),
     url(r'^post/$', views.addTrip, name='post'),
